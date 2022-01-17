f4436a4e19c2ef2eb7018fbc690f600a667b2a9a
MarkDVerhagen <markdverhagen@gmail.com>
2022-01-17 12:38:47 +0100

    New plots

---
+++ src/11_Virtue_1.R
@@ -0,0 +1,319 @@
+## Load packages
+
+packages <- c(
+  "tidyverse", "ggsci", "reshape2", "patchwork",
+  "cowplot", "scales", "grid", "gridExtra"
+)
+
+lapply(packages, require, character.only = TRUE)
+
+source("./src/functions.R")
+
+# Setup -------------------------------------------------------------------
+
+text_size <- 16
+
+# Figure 1A - Fragile Families --------------------------------------------
+font_family = "Bookman"
+theme_custom <- theme(panel.grid.major.x = element_line(size = 0.5, linetype = 'dotted',
+                                                        colour = "lightgrey"), 
+                      panel.grid.minor.x = element_line(size = 0.25, linetype = 'dotted',
+                                                        colour = "lightgrey"),
+                      # strip.placement = "outside", 
+                     #  strip.text.y = element_text(face = "bold", hjust=0.5, vjust=0.5),
+                      strip.background=element_rect(fill = NA, color = "black", size = 1.5),
+                      panel.spacing.x=unit(0.08, "lines"),
+                      panel.spacing.y=unit(0.1, "lines"),
+                      # panel.border = element_rect(color = "lightgrey", fill = NA, size = 0.5),
+                      panel.border = element_rect(color = "black"),
+                      legend.position = "top",
+                      text = element_text(size = 20, family = font_family),
+                      axis.text.x = element_text(size = 20),
+                      axis.text.y = element_text(size = 20))
+
+## Read data taken directly from the PNAS paper of the FFC.
+main_plot_ffc <- read.csv("data/ffc/main_plot.csv")
+
+ffc_df <- main_plot_ffc %>%
+  rename(outcomes = on,
+         best = point,
+         upper = ci.max,
+         lower = ci.min,
+         bench = benchmark) %>%
+  dplyr::select(outcomes, best, upper, lower, bench)
+
+ffc_df$outcomes <- factor(ffc_df$outcomes,
+                          levels = ffc_df$outcomes[order(ffc_df$bench, decreasing = T)])
+
+ffc_df$label <- fix_label(as.character(ffc_df$bench))
+
+## Generate Figure 1.A
+fig_1a <- ggplot(ffc_df, aes(y = bench, x = outcomes)) +
+  geom_bar(stat = "identity", fill = MetBrewer::met.brewer("Egypt")[2],
+           color = "black",
+           alpha = 0.8) +
+  geom_text(label = fix_label(paste0(round(ffc_df$bench, 3) * 100, "%")), vjust = -1,
+            size = 5, family = font_family) +
+  geom_rect(aes(ymin = rep(0.41, 6), ymax = rep(0.51, 6),
+         xmin = (seq(0.5, 5.5, 1) + 0.03), xmax = (seq(1.5, 6.5, 1) - 0.03)),
+         fill =  "white", colour = "black") +
+  geom_text(
+         label = ffc_df$outcomes,
+         size = 4.75, aes(y = 0.46, x = outcomes), family = font_family
+  ) +
+  theme_cowplot() + ylab("R-squared") + xlab("") +
+theme_custom +
+  scale_x_discrete(labels = c("Material hardship", "GPA", "Job training",
+                              "Eviction", "Grit", "Layoff")) +
+  theme_custom +
+         scale_y_continuous(labels = c("0%", "10%", "20%", "30%", "40%", "50%"),
+         breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5), limits = c(0, 0.51)) +
+         theme(
+                axis.text.x = element_blank(),
+                axis.ticks.x = element_blank()
+         )
+
+## 1B - Fragile Families detail of GPA predictions ------------------------
+
+## Read train and test set from FFC reproduction package
+df_train <- read.csv("data/ffc/train.csv")
+df_test <- read.csv("data/ffc/test.csv")
+
+ffc_gpa_full <- readRDS("data/edit/ffc_gpa_predictions_baseline_full.rds")
+
+ffc_gpa_demog_ols <- ffc_gpa_full[
+       ffc_gpa_full$account == "benchmark_ols_demographic",
+]
+
+ffc_gpa_full_ols <- ffc_gpa_full[
+       ffc_gpa_full$account == "benchmark_ols_full",
+]
+
+results <- data.frame(
+       test = ffc_gpa_demog_ols$truth,
+       nul = mean(ffc_gpa_demog_ols$truth, na.rm = T),
+       lm1 = ffc_gpa_demog_ols$prediction,
+       lm2 = ffc_gpa_full_ols$prediction,
+       challengeId = ffc_gpa_demog_ols$challengeID
+)
+
+## Transform predictions for plotting
+results_melt <- reshape2::melt(results, id.vars = c("test", "nul", "challengeId")) %>%
+       mutate(
+              variable = gsub("lm1", "Household", variable),
+              variable = gsub("lm2", "Household +\nlagged GPA", variable)
+       )
+
+## Save for Figure 2.B
+saveRDS(results_melt, "data/edit/ffc_gpa_predictions_null_baseline.rds")
+
+## Generate Figure 1.B
+
+fig_1b <- ggplot(results_melt, aes(y = value, x = test, fill = variable)) +
+  geom_jitter(
+         colour = "#333333",
+         pch=21,
+         size = 3, alpha = 0.8, width = 0.05) + #alpha = 0.2, size = 3, width = 0.04) +
+  geom_hline(aes(yintercept = nul), linetype = "dashed") + cowplot::theme_cowplot() +
+         theme_custom +
+  facet_grid(rows = vars(variable)) + xlim(1, 4) + ylim(1,4) + geom_abline(alpha = 0.7, linetype = "dotted") + theme_custom +
+  xlab("Observed GPA") + ylab("Predicted GPA") + theme(legend.position = "none") +
+  scale_fill_manual(values = MetBrewer::met.brewer("Ingres")[c(4, 5)])
+
+## New Fig 1C
+
+# results <- readRDS("./data/edit/mortgage_1c_results.rds")
+load("data/mortgage_temp.rda")
+
+dim(w_df)
+
+names(nw_df) <- paste0(names(nw_df), "_", "b")
+names(w_df) <- paste0(names(w_df), "_", "w")
+results <- cbind(overall_df, nw_df, w_df)
+
+results_melt <- results %>%
+       reshape2::melt() %>%
+       mutate(
+              var_name = gsub("_.*", "", variable),
+              sub = ifelse(grepl("_b", variable), "Black",
+                     ifelse(grepl("_w", variable), "White",
+                            ifelse(grepl("black", variable), "Black",
+                                   ifelse(grepl("white", variable), "White", "All")
+                            )
+                     )
+              )
+       )
+
+
+results_melt$var_name = factor(results_melt$var_name, levels = c("m0", "m1", "m2",
+                                                                 "m3", "m4", "m5"))
+results_group <- results_melt %>%
+  group_by(variable) %>%
+  summarise(mean = mean(value),
+            sd = sd(value)) %>%
+  mutate(p5 = mean - 1.96 * sd,
+         p95 = mean + 1.96 * sd) %>%
+  mutate(var_name = gsub("_.*", "", variable),
+         sub = ifelse(grepl("_b", variable), "Black",
+                      ifelse(grepl("_w", variable), "White",
+                             ifelse(grepl("_f", variable), "Female",
+                                    ifelse(grepl("_m", variable), "Male", "All")))),
+   
+         model_type = ifelse(grepl("joint", var_name), "Separate\nmodels", "One model"))
+
+add_all_mean <- results_group[results_group$sub == "All", c("var_name", "mean")] %>%
+  rename(all_mean = mean)
+
+results_group <- results_group %>%
+  left_join(add_all_mean, by = "var_name")
+
+results_group$label <- paste0(round(results_group$mean, 3) * 100, "%")
+
+rel_df <- results_group %>% filter(grepl("All|White|Black", sub)) %>% filter(!grepl("m2|m3|m5", var_name))
+rel_df$var_name <- c(rep(c("Null\nModel", "+ Objective\nScores", "+ Applicant\nRace"), 3))
+
+## Order for plotting
+rel_df$var_name <- factor(rel_df$var_name, levels = c("Null\nModel", "+ Objective\nScores", "+ Applicant\nRace"))
+rel_df$label <- fix_label(rel_df$label)
+
+fig_1c <- ggplot(rel_df %>% filter(sub %in% c("White", "Black"))) +
+  geom_bar(stat = "identity", position = "dodge2", color = "black", alpha = 0.8, aes(y = mean, x = fct_rev(as.factor(var_name)), fill = sub)) +
+  geom_text(aes(label = label, y = 1.0125, x = var_name, fill = sub), size = 5, position = position_dodge(width = .9), width = .2, family = font_family) +
+  geom_errorbar(aes(y = mean, x = fct_rev(as.factor(var_name)), ymin = p5, ymax = p95, fill = sub), position = position_dodge(width = .9), width = .2) +
+  geom_point(data = rel_df %>% filter(!(sub %in% c("White", "Black"))), aes(y = mean, x = fct_rev(as.factor(var_name)), shape = "Overall"),
+             size = 4, width = 2, stroke = 1) +
+  geom_text(data = rel_df %>% filter(!(sub %in% c("White", "Black"))),
+            aes(label = label, y = mean + 0.075, x = fct_rev(as.factor(var_name)), shape = "Overall"),
+            size = 4, width = 2, stroke = 1) +
+  coord_flip() + 
+  scale_y_continuous(labels = scales::percent) +
+  cowplot::theme_cowplot() +
+  scale_fill_manual(name = "", values = MetBrewer::met.brewer("VanGogh1")[c(1, 7)], labels = c("White subset", "Non-White subset")) +
+  scale_shape_manual(name = "", values = c(8)) +
+  ylab("") + theme_custom +
+  xlab("Explanatory Variables") +
+  theme(axis.text.x = element_text(size = (text_size - 1)),
+        axis.text.y = element_text(size = (text_size - 1))
+        ) +
+  theme(
+         legend.text = element_text(size = 15),
+         legend.title = element_text(size = 14)
+  ) + ylim(0, 1.03)
+  
+## Figure 1D
+
+source("data/teacher_bias/analysis_functions.R")
+source("data/teacher_bias/plot_functions.R")
+
+## load coefficients
+load("data/teacher_bias/coefs_perfs.rda")
+
+results <- rbind(readRDS("data/teacher_bias/1_50_lm_lme_op_mop_perf.rds"),
+                 readRDS("data/teacher_bias/51_250_lm_lme_op_mop_perf.rds"))
+
+results_full <- rbind(readRDS("data/teacher_bias/1_50_mop_full_perf.rds"),
+                      readRDS("data/teacher_bias/51_100_mop_full_perf.rds"),
+                      readRDS("data/teacher_bias/101_150_mop_full_perf.rds"),
+                      readRDS("data/teacher_bias/151_250_mop_full_perf.rds"))
+
+results_df <- as.data.frame(cbind(results, results_full))
+
+perf <- delist_perf(results_df)
+
+melted <- reshape2::melt(perf %>%
+                           dplyr::select(lm_g, lm_gender, lm_ses, lme_g,
+                                         op_g, op_gender, op_ses, mop_g))
+
+means <- melted %>% group_by(variable) %>% summarise(mean = mean(value))
+
+desc_df <-  melted %>%
+  group_by(variable) %>%
+  summarise(mean = mean(value),
+            sd = sd(value),
+            ptile_5 = quantile(value, probs=0.05, na.rm=TRUE),
+            ptile_95 = quantile(value, probs=0.95, na.rm=TRUE)) %>%
+  ungroup() %>%
+  mutate(model = ifelse(grepl("lm", variable), "Interval", "Categorical"),
+         type = ifelse(grepl("lme_|mop_", variable), "Including random intercept",
+                       ifelse(grepl("full", variable), "Full random effects", "No random effects")),
+         spec = ifelse(grepl("ses", variable), "+ Parental\nEducation",
+                       ifelse(grepl("gender", variable), "+ Sex",
+                              ifelse(grepl("school", variable), "+ School\nEffects", "Ability only"))))
+
+desc_df_full <- rbind(desc_df, desc_df[desc_df$variable %in% c("mop_g", "lme_g"), ] %>%
+                        mutate(variable = gsub("lme_g", "lm_school", variable),
+                               variable = gsub("mop_g", "op_school", variable)) %>%
+                        mutate(spec = "+ School\nEffects",
+                               type = "No random effects")) %>%
+  mutate(ifelse(grepl("op", variable), "Ordered Probit", "Linear Model")) %>%
+  mutate(ptile_95 = ifelse(model == "Interval", NA, ptile_95),
+         ptile_5 = ifelse(model == "Interval", NA, ptile_5))
+
+desc_df_full$model <- factor(desc_df_full$model, levels = c("Interval", "Categorical"))
+desc_df_full$label <- paste0(round(desc_df_full$mean, 3) * 100, "%")
+text_size <- 16
+
+plot_2c_df <- desc_df_full %>% filter(!grepl("Full", type), 
+                                      type == "No random effects",
+                                      !grepl("Sex", spec))
+
+plot_2c_df$spec <- factor(plot_2c_df$spec, levels = unique(plot_2c_df$spec))
+plot_2c_df$model <- factor(plot_2c_df$model, levels = c("Categorical", "Interval"))
+plot_2c_df$label <- fix_label(plot_2c_df$label)
+
+fig_2c <- ggplot(plot_2c_df) +
+  geom_bar(stat = "identity", position = "dodge2", color = "black", alpha = 0.8, aes(x = fct_rev(as.factor(spec)), y = mean, fill = model)) +
+  geom_text(aes(label = label, y = 1.0125, x = fct_rev(as.factor(spec)), fill = model),
+            size = 5, position = position_dodge(width = .9), width = .2, family = font_family) +
+  geom_errorbar(aes(y = mean, x = fct_rev(as.factor(spec)), ymin = ptile_5, ymax = ptile_95, fill = model),
+                position = position_dodge(width = .9), width = 0.2) +
+  coord_flip() +
+  scale_y_continuous(labels = scales::percent) +
+  cowplot::theme_cowplot() +
+  scale_fill_manual(name = "", values = MetBrewer::met.brewer("Lakota")[c(3, 6)],
+  labels = c("Categorical", "Interval")) +
+  ylab("Predictive Accuracy") +
+  theme_custom + 
+  geom_hline(yintercept = 0, linetype = 2, color = "darkgrey") +
+  xlab("Explanatory Variables") + 
+  theme(axis.text.x = element_text(size = (text_size - 1)),
+        axis.text.y = element_text(size = (text_size - 1))) +
+  theme(legend.text = element_text(size = 15),
+        legend.title = element_text(size = 14)) +
+         ylim(0, 1.03)
+
+fig_2c
+
+load("data/lemieux/results.rda")
+
+fig_1d_df <- melt(resid, id.vars = "year") %>% mutate(variable = gsub("explained", "Explained", variable),
+                                                      variable = gsub("resid", "Unexplained", variable),
+                                                      year = year + 1900)
+
+fig_1d <- ggplot(fig_1d_df) +
+  geom_area(aes(x = year, y = value, fill = variable), color = "black") +
+  scale_fill_manual(name = "", values = MetBrewer::met.brewer("Cassatt1", n = 8)[c(2, 7)]) +
+  theme_bw() +
+  geom_point(data = df_year_r2, aes(x = year, y = r2, shape = "1973 model"), size = 3, fill = "black") +
+  geom_point(data = df_year_r2, aes(x = year, y = r2_own, shape = "Year model"), size = 3) +
+  scale_shape_manual(name = "", values = c(8, 6)) +
+  cowplot::theme_cowplot() +
+  theme_custom +
+  xlab("Year") +
+  scale_y_continuous("Variance / R-squared") +
+  theme(axis.text.x = element_text(size = (text_size - 1)),
+        axis.text.y = element_text(size = (text_size - 1))
+        ) +
+  theme(legend.text = element_text(size = 15),
+        legend.title = element_text(size = 14))
+
+## Combine plots
+patchwork <- (fig_1a / fig_1c / fig_2c) | (fig_1b / fig_1d)
+
+ggsave("tex/figs/fig1_external_consciousness_new.pdf",
+       patchwork +
+         plot_annotation(tag_levels = list(c('A', 'C', 'D', 'B', 'E')), tag_suffix = '.') &
+         theme(text = element_text("serif"),
+               plot.tag = element_text(face = 'bold')),
+       width = 16, height = 14
+)
---
+++ src/13_Virtue_3.R
@@ -0,0 +1,231 @@
+packages <- c("tidyverse", "patchwork", "cowplot",
+              "ggsci")
+lapply(packages, require, character.only = TRUE)
+
+source("src/functions.R")
+
+# Setup plotting themes --------------------------------------------------------
+text_size = 16
+font_family = "Bookman"
+theme_custom <- theme(
+  panel.grid.major.x = element_line(
+    size = 0.5, linetype = "dotted",
+    colour = "lightgrey"
+  ),
+  panel.grid.minor.x = element_line(
+    size = 0.25, linetype = "dotted",
+    colour = "lightgrey"
+  ),
+  # strip.placement = "outside",
+  strip.text.y = element_text(face = "bold", hjust = 0.5, vjust = 0.5),
+  strip.background = element_rect(fill = NA, color = "black", size = 1.5),
+  panel.spacing.x = unit(0.08, "lines"),
+  panel.spacing.y = unit(0.1, "lines"),
+  # panel.border = element_rect(color = "lightgrey", fill = NA, size = 0.5),
+  panel.border = element_rect(color = "black"),
+  legend.position = "top",
+  text = element_text(size = 20, family = font_family),
+  axis.text.x = element_text(size = 20),
+  axis.text.y = element_text(size = 20)
+)
+
+# Mortgage Discrimination -------------------------------------------------
+
+load("data/mortgage_temp.rda")
+nw_pred_melt <- nw_pred %>%
+  reshape2::melt()
+w_pred_melt <- w_pred %>%
+  reshape2::melt()
+total_pred_melt <- rbind(nw_pred_melt, w_pred_melt) %>%
+  mutate(group = ifelse(variable %in% c("w_w", "w_nw"), "White", "Non-White"),
+         variable = ifelse(variable %in% c("w_w", "nw_nw"), "Original Data", "Intervened Data"))
+
+mean_total <- total_pred_melt %>%
+  group_by(variable, group) %>%
+  summarise(value = mean(value))
+
+total_pred_melt$variable <- factor(total_pred_melt$variable,
+levels = c("Original Data", "Intervened Data")
+)
+mean_total$variable <- factor(mean_total$variable,
+levels = c("Original Data", "Intervened Data")
+)
+
+prob_plot <- ggplot(total_pred_melt) +
+  geom_jitter(aes(y = value, x = variable, color = variable, fill = variable), width = 0.42,
+  colour = "#333333", pch=21,
+  size = 3, alpha = 0.9) +
+  geom_violin(aes(y = value, x = variable, fill = variable), alpha = 0.8,
+  color = "black"
+  ) +
+    geom_point(data = mean_total, aes(y = value, x = variable), shape = 8, size = 5) +
+      geom_segment(data = mean_total, aes(
+        x = c(1.5, 1.5, 0.5, 0.5), xend = c(2.5, 2.5, 1.5, 1.5), y = value,
+        yend = value, group = group
+      )) +
+      geom_text(data = mean_total, aes(
+        label = fix_label(paste0(round(value * 100, 1), "%")), group = group, x = variable, y = 1.02
+      ), size = 4.5, family = font_family
+      ) +
+    facet_wrap(~group) +
+      cowplot::theme_cowplot() +
+      theme_custom +
+      ylab("Predicted probability of success") +
+      xlab("") +
+      scale_y_continuous(labels = c("0%", "25%", "50%", "75%", "100%"),
+      breaks = c(0, 0.25, 0.5, 0.75, 1), limits = c(0, 1.02)) +
+      scale_color_manual(name = "", values = ggsci::pal_aaas("default")(4)[3:4]) +
+      scale_fill_manual(name = "", values = ggsci::pal_aaas("default")(4)[3:4]) +
+      theme(
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()
+        # axis.text.y = element_text(size = 10)
+      ) + guides(fill = guide_legend(override.aes = list(shape = NA, alpha = NA)))
+
+plot_df <- do_df %>%
+  reshape2::melt() %>%
+  rename(success = value) %>%
+  mutate(failure = 1 - success) %>%
+  group_by(variable) %>%
+  summarise(p5_success = quantile(success, 0.05),
+            p95_success = quantile(success, 0.95),
+            p5_failure = quantile(failure, 0.05),
+            p95_failure = quantile(failure, 0.95),
+            success = mean(success),
+            failure = mean(failure)) %>%
+  mutate(
+      model = ifelse(variable %in% c("nw_nw", "w_w"), "Original Data", "Intervened Data"),
+      race = ifelse(grepl("^nw", variable), "Non-White", "White")
+  )
+
+final_plot_df <- rbind(plot_df[,
+c("variable", "success", "p5_success", "p95_success", "model", "race")
+] %>%
+                         rename(value = success,
+                                p5 = p5_success,
+                                p95 = p95_success) %>%
+                         mutate(x = "Accepted"),
+                       plot_df[, c("variable", "failure", "p5_failure",
+                       "p95_failure", "model", "race")] %>%
+                         rename(value = failure,
+                                p5 = p5_failure,
+                                p95 = p95_failure) %>%
+                         mutate(x = "Denied"))
+
+final_plot_df$x <- factor(final_plot_df$x, levels = c("Accepted", "Denied"))
+final_plot_df$label <- paste0(round(final_plot_df$value, 3) * 100, "%")
+final_plot_df$label <- fix_label(final_plot_df$label)
+final_plot_df$model <- factor(final_plot_df$model,
+  levels = c("Original Data", "Intervened Data")
+)
+text_size_small <- 4.5
+
+outcome_plot <- ggplot(
+  final_plot_df %>% filter(x == "Accepted"),
+  aes(x = 1, y = value, fill = model)
+) +
+  geom_bar(stat = "identity", position = position_dodge(),
+  color = "black",
+  alpha = 0.9) +
+  geom_errorbar(aes(ymin = p5, ymax = p95), width = 0.2,
+  position = position_dodge(width=0.9)) +
+  geom_text(aes(label = label, y = p95 + 0.02),
+    size = text_size_small, family = font_family,
+    position = position_dodge(width = 0.9)
+  ) +
+    facet_wrap(~race) +
+  # coord_flip() +
+  cowplot::theme_cowplot() +
+  scale_y_continuous(labels = c("0%", "25%", "50%", "75%", "100%"),
+  breaks = c(0, 0.25, 0.5, 0.75, 1), limits = c(0, 1.02)) +
+  theme_custom + scale_fill_manual(name = "", values = ggsci::pal_aaas("default")(4)[3:4]) +
+    ylab("Percentage predicted succesfull") +
+    xlab("") +
+    theme(
+      # axis.text.y = element_text(size = 10),
+      axis.ticks.x = element_blank(), axis.text.x = element_blank()
+    ) +
+    guides(fill = guide_legend(override.aes = list(shape = NA, alpha = NA)))
+
+## -- Figure 3C
+
+cross_results_df <- readRDS("data/teacher_bias/1_250_cross_within.rds")
+# cross_results_df <- readRDS("data/simulations/1_250_cross_within_resample.rds")
+
+names(cross_results_df) <- c("ses_00", "ses_10", "ses_01", "ses_11", "sex_00", "sex_10", "sex_01", "sex_11")
+
+df_res <- lapply(cross_results_df, FUN = function(x) delist_cross(x, col_names = c("I", "II", "III", "IV", "V")))
+
+
+cross_plot <- function(df1, df2, theme_custom, first = T, color1 = "#EE0000FF", color2 = "#3B4992FF", font_family = "Bookman") {
+  text_size = 5.5
+  df_own <- df1
+  df_other <- df2
+  if (mean(df_own %*% c(1, 2, 3, 4, 5)) > mean(df_other %*% c(1, 2, 3, 4, 5))) {
+    offset = 1  
+  } else {
+    offset = -1
+  }
+  
+  
+  plot_df <- data.frame(df_own, df_other) %>%
+    reshape2::melt() %>%
+    group_by(variable) %>%
+    summarise(mean = mean(value),
+              ptile_5 = quantile(value, probs=0.05, na.rm=TRUE),
+              ptile_95 = quantile(value, probs=0.95, na.rm=TRUE)) %>%
+    mutate(model = ifelse(grepl("\\.1", variable), "Other model", "Own model"),
+           x = gsub("\\.1", "", variable))
+  
+  ggplot(plot_df, aes(x = x, y = mean, fill =model)) +
+    geom_bar(stat = "summary", position = position_dodge(),
+    color = "black"
+    ) +
+    geom_errorbar(aes(ymin = ptile_5, ymax = ptile_95), width = 0.2, position = position_dodge(width=1)) +
+    geom_text(aes(label = paste0(round(mean, 3) * 100, "%")), position = position_dodge(width = 1), hjust=-.4, size=text_size, family = font_family) +
+    coord_flip() + scale_fill_aaas(name = "Prediction model") + cowplot::theme_cowplot() +
+    geom_vline(xintercept = mean(df_own %*% c(1, 2, 3, 4, 5)), linetype="dashed",
+               color = color1) +
+    annotate("text",
+      x = mean(df_own %*% c(1, 2, 3, 4, 5)) + 0.2 * offset, y = 0.6, color = color1, size = text_size,
+      label = as.character(round(mean(df_own %*% c(1, 2, 3, 4, 5)), 3)),
+      family = font_family
+    ) +
+      theme_custom +
+    geom_vline(xintercept = mean(df_other %*% c(1, 2, 3, 4, 5)), linetype = "dashed",
+               color = color2) +
+    annotate("text", x = mean(df_other %*% c(1, 2, 3, 4, 5)) - 0.2 * offset, y = 0.6, color = color2, size=text_size,
+             label = as.character(round(mean(df_other %*% c(1, 2, 3, 4, 5)), 3))) +
+    theme(legend.position = "bottom",
+          panel.border = element_rect(color = "black")
+          # text = element_text(size = 14),
+          # axis.text.x = element_text(size = 14), axis.text.y = element_text(size = 14)
+    ) +
+    scale_y_continuous(labels = scales::percent, limits = c(0, 0.65)) +
+    xlab("Predicted track level") + ylab("Proportion of predictions assigned to track")
+}
+
+
+ses_0 <- cross_plot(df_res$ses_00, df_res$ses_10, theme_custom = theme_custom) + scale_fill_aaas(name = "", labels = c("Predicted as low Parent. Educ.", "Predicted as high Parent. Educ.")) +
+  theme(legend.position = "none") + ylab("")
+ses_1 <- cross_plot(df_res$ses_01, df_res$ses_11, theme_custom = theme_custom) + scale_fill_aaas(name = "", labels = c("Predicted as low Parent. Educ.", "Predicted as high Parent. Educ."))
+
+plot_3b <- ((ses_0 + ggtitle("High Parental Education students")) / (ses_1 + ggtitle("Low Parental Education students")) + plot_layout(ncol = 1, guides = "collect") +
+               plot_annotation(theme = theme(legend.position = "bottom")))
+
+# Combine -----------------------------------------------------------------
+
+plot_3a <- (prob_plot + plot_layout(guides = "collect") & theme(legend.position = "bottom")) +
+  ((outcome_plot) + theme(legend.position = "none"))
+
+patchwork <- plot_3a | plot_3b
+patchwork +
+plot_layout(widths = c(5, 3)) +
+plot_annotation(
+  tag_levels = list(c('A.', ' ', 'B.', ' '))) &
+         theme(text = element_text("serif"),
+               plot.tag = element_text(face = 'bold'))
+
+ggsave("tex/figs/fig3_group_analysis_new.pdf", last_plot(), height = 10, width = 16)
+# ggsave("tex/figs/fig3_group_analysis.jpg", last_plot(), height = 10, width = 16)
+
